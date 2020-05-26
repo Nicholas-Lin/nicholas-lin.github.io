eee2204bbf63b30c3749acdc316fff115bfd611e
Nicholas Lin <nicholaslin10@gmail.com>
2020-05-25 23:54:31 -0400

    Finished skills

--- custom/js/scrolling-nav.js
+++ custom/js/scrolling-nav.js
@@ -33,6 +33,23 @@ window.addEventListener("scroll", function(){
   nav.classList.toggle("scrolled", window.scrollY >$(window).height());
 });
 
+//TESTING ACTIVE SCROLL
 
+$(document).ready(function() {
+  $(document).on("scroll", onScroll);
 
-
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('nav a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('a').removeClass("active");
+          currLink.addClass("active");
+      }
+      else{
+          currLink.removeClass("active");
+      }
+      });
+  }
+})
